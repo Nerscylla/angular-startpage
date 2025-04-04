c5fa1da04d6a1d3cbfc94f993ccbf3169fa7e604
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2025-04-04 11:39:47 +0200

    feat: implement search engine selection in config-engine component

--- src/app/config/components/config-engine/config-engine.component.ts
+++ src/app/config/components/config-engine/config-engine.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { getConfig, setConfig } from '../../../helpers/configurationHelper';
+import { FormsModule } from '@angular/forms';
+import { barData } from '../../../../../public/barData.json';
+const { engines } = barData;
 
 @Component({
 	selector: 'config-engine',
-	imports: [],
+	imports: [FormsModule, CommonModule],
 	templateUrl: './config-engine.component.html',
 	styleUrl: './config-engine.component.scss',
 })
-export class ConfigEngineComponent {}
+export class ConfigEngineComponent implements OnInit {
+	engine: string = '';
+	enginesArr: string[] = this.getEnginesKeys();
+
+	getEnginesKeys(): string[] {
+		return Object.keys(engines);
+	}
+
+	updateEngine() {
+		setConfig('preferedSearchEngine', this.engine);
+	}
+
+	ngOnInit() {
+		this.engine = getConfig('preferedSearchEngine');
+	}
+}

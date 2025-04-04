698e092d5dd0d5d6760f9ddf38cf7d1181648328
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2025-04-04 12:21:46 +0200

    fix: correct order of configuration helper functions for clarity

--- src/app/helpers/configurationHelper.ts
+++ src/app/helpers/configurationHelper.ts
@@ -1,17 +1,22 @@
+// initially get configuration
 let configuration: { [key: string]: any } = JSON.parse(
 	localStorage.getItem('angular-startpage:config') || '{}'
 );
 
+// get specifig key from configuration
+let getConfig: Function = (configKey: string): any => {
+	return configuration[configKey];
+};
+
+// set configuration by key
 let setConfig: Function = (configKey: string, configVal: any): void => {
+	// set temporary
 	configuration[configKey] = configVal;
+	// store in localStorage
 	localStorage.setItem(
 		'angular-startpage:config',
 		JSON.stringify(configuration)
 	);
-};
-
-let getConfig: Function = (configKey: string): any => {
-	return configuration[configKey];
 };
 
 export { setConfig, getConfig };

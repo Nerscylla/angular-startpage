5585809c2916ad4a23a5b647b0fa0a53fe3f2343
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2025-04-07 22:27:16 +0200

    refractor: clean up a log, doc and seperate funcs

--- src/app/helpers/searchHelper.ts
+++ src/app/helpers/searchHelper.ts
@@ -1,60 +1,84 @@
 import { barData } from '../../../public/barData.json';
 const { bangs, bangsSearch, engines } = barData;
-import { getConfig, setConfig } from './configurationHelper';
+import { getConfig } from './configurationHelper';
 
 let preferedSearchEngine: string =
 	getConfig('preferedSearchEngine') || 'duckduckgo';
 
 // temporary shit
-let redditSearch: boolean = true;
+let redditRedirect: boolean = getConfig('redditRedirects') || false;
 
+// main search
 let search = (searchString: string) => {
-	// duckduckgo-ish bangs
-	if (!Object.keys(engines).includes(preferedSearchEngine)) {
-		setConfig('preferedSearchEngine', 'duckduckgo');
-	}
-	if (searchString.startsWith('!')) {
-		searchString = searchString.replace('!', '');
-		let SplitSearchString: string[] = searchString.split(' ');
-		if (SplitSearchString.length > 1) {
-			if (SplitSearchString[0] in bangsSearch || SplitSearchString[0] in engines) {
-				let firstWord = SplitSearchString.shift();
-				let urlExtension: string = SplitSearchString.join('%20');
-				let urlBase: string = '';
-				if (firstWord) {
-					urlBase =
-						bangsSearch[firstWord as keyof typeof bangsSearch] ||
-						engines[firstWord as keyof typeof engines];
-				}
-				return urlBase + urlExtension;
-			} else {
-				return engines[preferedSearchEngine as keyof typeof engines] + searchString;
-			}
-		} else {
-			if (searchString in bangs) {
-				return bangs[searchString as keyof typeof bangs];
-			} else {
-				return engines[preferedSearchEngine as keyof typeof engines] + searchString;
-			}
-		}
+	// check for valid uri first
+	if (
+		!/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(
+			searchString
+		)
+	) {
+		// go on to bangs if there is no uri
+		return bangsChecking(searchString);
 	} else {
-		if (
-			/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(
-				searchString
-			)
-		) {
-			return searchString.startsWith('http')
-				? searchString
-				: `https://${searchString}`;
-		}
-		if (
-			(searchString.startsWith('r/') && redditSearch) ||
-			(searchString.startsWith('u/') && redditSearch)
-		) {
-			return `https://www.reddit.com/${searchString}`;
-		}
-		return engines[preferedSearchEngine as keyof typeof engines] + searchString;
+		// add https:// if !http && !https present
+		return searchString.startsWith('http') || searchString.startsWith('https')
+			? searchString
+			: `https://${searchString}`;
 	}
+};
+
+let bangsChecking = (searchString: string) => {
+	// check for bang start
+	if (!searchString.startsWith('!')) {
+		// go on to reddit checks if no bangs are present
+		return redditLinking(searchString);
+	}
+	// edit bangs like required
+	let modSearchString: string = searchString.replace('!', '');
+	let searchStringArr: string[] = modSearchString.split(' ');
+	// word count
+	if (searchStringArr.length > 1) {
+		// bangs search
+		return bangsSearchFunc(searchStringArr, searchString);
+	} else {
+		// bangs links
+		return bangsLinkFunc(modSearchString, searchString);
+	}
+};
+
+let bangsSearchFunc = (searchStringArr: string[], searchString: string) => {
+	// go reddit if there is no valid bangs search
+	if (!bangsSearch[searchStringArr[0] as keyof typeof bangsSearch]) {
+		return redditLinking(searchString);
+	} else {
+		// go to searchable bangs
+		let firstWord: any = searchStringArr.shift();
+		let extension: string = searchStringArr.join('%20');
+		return bangsSearch[firstWord as keyof typeof bangsSearch] + extension;
+	}
+};
+
+let bangsLinkFunc = (bang: string, searchString: string) => {
+	if (bangs[bang as keyof typeof bangs]) {
+		// go to link if present
+		return bangs[bang as keyof typeof bangs];
+	} else {
+		// go to reddit if link isn't available
+		return redditLinking(searchString);
+	}
+};
+
+let redditLinking = (searchString: string) => {
+	if (!redditRedirect) {
+		return defaultEngineSearch(searchString);
+	}
+	if (searchString.startsWith('r/') || searchString.startsWith('u/')) {
+		return `https://www.reddit.com/${searchString}`;
+	}
+	return defaultEngineSearch(searchString);
+};
+
+let defaultEngineSearch = (searchString: string) => {
+	return engines[preferedSearchEngine as keyof typeof engines] + searchString;
 };
 
 export { search };

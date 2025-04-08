cab1bb33423eb05cd59dd1dd6d275758df22633f
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2025-04-08 12:41:11 +0200

    feat: implement wallpaper management in home page component

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClockComponent } from './components/clock/clock.component';
 import { SearchBarComponent } from './components/searchBar/searchBar.component';
+import { setConfig, getConfig } from '../helpers/configurationHelper';
 
 @Component({
 	selector: 'app-home-page',
@@ -8,4 +9,27 @@ import { SearchBarComponent } from './components/searchBar/searchBar.component';
 	templateUrl: './home-page.component.html',
 	styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+	wallpaperUrls: string[] = [];
+	wallpaperIndex: number = 0;
+
+	ngOnInit(): void {
+		this.wallpaperIndex = getConfig('wallpaperCurrentIndex') || 0;
+		this.wallpaperUrls = getConfig('wallpaperUrls');
+		if (this.wallpaperUrls) {
+			this.setBodyBackground();
+		}
+	}
+
+	setBodyBackground() {
+		this.wallpaperIndex = ++this.wallpaperIndex % this.wallpaperUrls.length;
+		this.applyBodyBackground(this.wallpaperUrls[this.wallpaperIndex]);
+		setConfig('wallpaperCurrentIndex', this.wallpaperIndex);
+	}
+
+	applyBodyBackground(url: string) {
+		document.body.style.backgroundImage = `url('${url}')`;
+		document.body.style.backgroundSize = 'cover';
+		document.body.style.backgroundPosition = 'center';
+	}
+}

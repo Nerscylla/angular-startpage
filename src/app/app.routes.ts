376df3e389c96d20ed162901115e208ce1042305
TheBaum <the.baum12345@gmail.com>
2025-04-27 20:07:58 +0200

    fix: some issues i had with the routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,12 @@ import { ConfigComponent } from './config/config.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
-	{ path: 'config', component: ConfigComponent },
-	{ path: 'angular-startpage/config', component: ConfigComponent },
-	{ path: '**', component: HomePageComponent },
+	{ path: 'config', component: ConfigComponent }, // Route for /config
+	{ path: 'configure', component: ConfigComponent }, // Route for /configure
+	{ path: 'configuration', component: ConfigComponent }, // Route for /configuration
+	{ path: ':/config', component: ConfigComponent }, // Route for /:config (dynamic segment)
+	{ path: ':/configure', component: ConfigComponent }, // Route for /:configure (dynamic segment)
+	{ path: ':/configuration', component: ConfigComponent }, // Route for /:configuration (dynamic segment)
+	{ path: '', component: HomePageComponent }, // Default route for the home page
+	{ path: '**', component: HomePageComponent }, // Catch-all route for undefined paths
 ];
